2d951d102b7c96caa7c73c82d9a3bdd233507563
siddiquenaim <naim.niter.tex06@gmail.com>
2023-07-25 21:04:55 +0600

    Solved task-4

--- task-solutions.js
+++ task-solutions.js
@@ -52,12 +52,35 @@ const mostFrequentFinder = (array) => {
   return mostFrequent;
 };
 
-console.log(mostFrequentFinder(givenArray));
+// console.log(mostFrequentFinder(givenArray));
 /*
 Task 4: Create a function that takes a sorted array of numbers and a target value as input. The function should find two numbers in the array that add up to the target value. Return an array containing the indices of the two numbers.
 
 Example Input: ([1, 3, 6, 8, 11, 15], 9) Example Output: [1, 2] (numbers at indices 1 and 2: 3 + 6 = 9)
 */
+
+const sortedArray = [1, 3, 6, 8, 11, 15];
+const target = 9;
+
+const twoNumbers = (array, target) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    const sum = array[left] + array[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return null;
+};
+
+console.log(twoNumbers(sortedArray, target));
 
 /*
 Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
