6aa93a43b8d5ecf1ee0de322a13e9055723575d3
siddiquenaim <naim.niter.tex06@gmail.com>
2023-07-25 22:29:48 +0600

    Solved task-5

--- task-solutions.js
+++ task-solutions.js
@@ -80,11 +80,38 @@ const twoNumbers = (array, target) => {
   return null;
 };
 
-console.log(twoNumbers(sortedArray, target));
+// console.log(twoNumbers(sortedArray, target));
 
 /*
 Task 5: Implement a simple JavaScript calculator. The calculator should take two numbers and an operator (+, -, *, /) as input and return the result of the operation.
 */
+
+const num1 = 10;
+const num2 = 5;
+const operator = "-";
+
+const mathOperation = (num1, num2, operator) => {
+  if (
+    (typeof num1 === "number" &&
+      typeof num2 === "number" &&
+      operator === "+") ||
+    operator === "-" ||
+    operator === "*" ||
+    operator === "/"
+  ) {
+    if (operator === "+") {
+      return num1 + num2;
+    } else if (operator === "-") {
+      return num1 - num2;
+    } else if (operator === "*") {
+      return num1 * num2;
+    } else if (operator === "/") {
+      return num1 / num2;
+    }
+  }
+};
+
+console.log(mathOperation(num1, num2, operator));
 
 /*
 Task 6: Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
